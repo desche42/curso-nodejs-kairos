897fd32796d68a158eafe91213ccc247cb9d9f78
Alejandro Sánchez Pineda <alsapi2004@gmail.com>
2019-11-16 10:09:33 +0100

    express - rutas con parametros

--- clase2/app.js
+++ clase2/app.js
@@ -1,11 +1,27 @@
 const express = require('express');
+const path = require('path');
+
+// cosillas de seguridad
+const helmet = require('helmet');
 
 const app = express();
+app.use(helmet());
+
+// podemos definir variables globales a nivel de app
+app.locals.title  = 'My App';
+
+// definimos dónde vamos a tner las vistas
+app.set('views', path.join(__dirname, 'views'));
 
 // definimos la ruta index
 app.get('/', (req, res) => {
-  res.status(200).send('Hello world');
+  const { query } = req;
+  res.json(query)
 });
 
+app.get('/user/:id', (req, res) => {
+  const { id } = req.params;
+  res.send(id);
+});
 
-app.listen(3000);
+app.listen(3000, () => console.log('Listening on port 3000'));
